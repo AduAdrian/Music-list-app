4694cd54bfcaa15a47eb15025965437016d3f27f
AduAdrian <47608742+AduAdrian@users.noreply.github.com>
2019-03-03 19:18:22 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import {TextField, Button} from '@material-ui/core';
+import axios from 'axios';
 
+const API_URL = 'https://ws.audioscrobbler.com/2.0/?limit=5&format=json&method=artist.search&api_key=' + process.env.REACT_APP_LASTFM_APPKEY;
+
+const isEmplt = (str) => str.length === 0;
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+state = {
+searchTerm: '',
+artists: []
+}
+onTextChange = (event) => {
+const value = event.target.value;
+
+this.setState({searchTerm: value});
+}
+
+search = (terms) => {
+const request = API_URL + '&artist=' + terms;
+
+console.log(request);
+
+axios.get(request).then((response) => {
+this.setState({ artists: response.data.results.artistmatches.artist});
+})
+}
+
+onSearchClick = () => {
+this.search(this.state.searchTerm);
+}
+//https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=cher&api_key=d000cff27039229f64111c30e925a415&format=json
+
+
+render() {
+return (
+<div className="App">
+<header className="App-header">
+<AppBar position="static" color="primary">
+<Toolbar>
+<Typography variant="h6" color="inherit">
+Photos
+</Typography>
+<TextField 
+placeholder="Search on Spotify" 
+onChange={this.onTextChange}
+value={this.state.searchTerm}
+/>
+<Button 
+onClick={this.onSearchClick} 
+disabled={this.state.searchTerm.length === 0}
+>Search
+</Button>
+</Toolbar>
+</AppBar>
+</header>
+<ul>
+{this.state.artists.map((artist) => {
+return <li><span>{artist.name}</span></li>
+})}
+</ul>
+
+</div>
+);
+}
 }
 
 export default App;
